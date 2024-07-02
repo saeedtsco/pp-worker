2202b41c1fd56406bcb04d2e7948f6fcccab5a8a
saeedtsco <166684310+saeedtsco@users.noreply.github.com>
2024-07-02 05:41:34 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ import { connect } from 'cloudflare:sockets';
 // t.me/P_tech2024 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '2cd61524-9843-43ab-8a0a-df4a744bb859';
+let userID = '5b6c8f70-209e-4e3b-8267-191ba907a4af';
 
 const proxyIPs = ['mtn.ircf.space', 'mkh.ircf.space', 'mci.ircf.space', 'rtl.ircf.space'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
